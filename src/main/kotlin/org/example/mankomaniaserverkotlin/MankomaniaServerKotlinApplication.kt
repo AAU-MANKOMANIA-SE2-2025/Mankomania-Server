345752c6c251a6659a7f083139d7f1616078dfd9
MauMauChat <lucaan@edu.aau.at>
2025-03-24 00:15:19 +0100

    Add Project Structure

---
+++ src/main/kotlin/org/example/mankomaniaserverkotlin/MankomaniaServerKotlinApplication.kt
@@ -0,0 +1,11 @@
+package org.example.mankomaniaserverkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MankomaniaServerKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<MankomaniaServerKotlinApplication>(*args)
+}
