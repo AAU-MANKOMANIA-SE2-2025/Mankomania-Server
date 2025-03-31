b24c26cd1fd8a897fefeb436c42b35b613a6d2fd
eles17 <lev.starman8@gmail.com>
2025-03-31 22:19:15 +0200

    feat: add board wrap-around logic to Player movement + unit test to validate correct position after wrapping (#24)

--- app/src/test/java/com/example/mankomania_client/PlayerTest.kt
+++ app/src/test/java/com/example/mankomania_client/PlayerTest.kt
@@ -16,4 +16,18 @@ class PlayerTest {
         player.move(3)
         assertEquals(8,player.position)
     }
+
+    @Test
+    fun playerWrapsAroundBoardCorrectly (){
+        val player = Player(name = "blue", position = 30)
+        player.move(5)
+        assertEquals(3,player.position)
+    }
+
+    @Test
+    fun playerLandsExactlyOnLastField (){
+        val player = Player(name = "blue", position = 28)
+        player.move(4)
+        assertEquals(0,player.position) //32 % 32 = 0...
+    }
 }
--- src/main/kotlin/org/example/mankomaniaserverkotlin/ToBeMoved/Player.kt
+++ src/main/kotlin/org/example/mankomaniaserverkotlin/ToBeMoved/Player.kt
@@ -3,10 +3,12 @@ package com.example.mankomania_client.model
 data class Player(
     val name: String,
     var position: Int = 0,
-    var money: Int = 0) {
+    var money: Int = 0,
+    val maxFields: Int = 32 // placeholder
+    ) {
 
     fun move(steps:Int){
-        position += steps
+        position= (position + steps) % maxFields //if on 30 --> wrap around back to 1
     }
 
 
